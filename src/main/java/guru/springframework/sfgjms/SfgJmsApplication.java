a2cc6503c767ee73b7d6b44ddb68d64360cada73
Hicham <bahir.hicham@gmail.com>
2022-12-20 15:30:05 +0100

    Chapter 177

--- src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
+++ src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
@@ -1,12 +1,30 @@
 package guru.springframework.sfgjms;
 
+import org.apache.activemq.artemis.api.core.TransportConfiguration;
+import org.apache.activemq.artemis.core.config.Configuration;
+import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+import org.apache.activemq.artemis.core.remoting.impl.invm.InVMAcceptorFactory;
+import org.apache.activemq.artemis.core.remoting.impl.netty.NettyAcceptorFactory;
+import org.apache.activemq.artemis.core.server.ActiveMQServer;
+import org.apache.activemq.artemis.core.server.ActiveMQServers;
+import org.apache.activemq.artemis.core.server.impl.ActiveMQServerImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashSet;
 
 @SpringBootApplication
 public class SfgJmsApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+                .setPersistenceEnabled(false)
+                .setJournalDirectory("target/data/journal")
+                .setSecurityEnabled(false)
+                .addAcceptorConfiguration("invm", "vm://0"));
+        server.start();
+
         SpringApplication.run(SfgJmsApplication.class, args);
     }
 
